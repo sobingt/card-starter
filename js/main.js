9f2d0e65e33d6661dd6c68cebd652fad90bc3d25
Orville Fernandes <orville.fernandes@bitbrothers.in>
2015-11-16 18:29:16 +0530

    flipping card added

--- js/main.js
+++ js/main.js
@@ -1,38 +1,53 @@
-//Card object - template function
+//Card- template function
 var card = function(value, name, suit){
   this.value = value;
   this.name = name;
   this.suit = suit;
 };
 
-//Deck object - template function
+var heart2 = new card(2,'2','Hearts');
+
+//Deck - template function - just moulds
 var deck = function(){
   this.names = ['1','2','3','4','5','6','7','8','9','10','J','Q','K'];
   this.suits = ['Hearts','Diamonds','Spades','Clubs'];
   var cards = [];
-  
+  //52 loops = 4 *13
   for(var i = 0; i< this.suits.length;i++)
   {
     for(var j = 0; j< this.names.length;j++){
-      cards.push(new card(j+1, this.names[j], this.suits[i]));
-      console.log(cards);
+      var cardObject = new card(j+1, this.names[j], this.suits[i]);
+      cards.push(cardObject);
     }
       
   }
-
+  //deck of 52 card objects
   return cards;
 };
+
+
+var shuffle = function(deck){
+  for(var j, x, i = deck.length; i; j = parseInt(Math.random() * i), x = deck[--i], deck[i] = deck[j], deck[j] = x);
+  
+  return deck;
+};
+
 
 //Creating a deck object
 var myDeck = new deck();
 
 window.onload = function(){
 
+  shuffle(myDeck);
   //Loop for all 52 cards in the deck
   for(var i = 0; i< myDeck.length; i++)
   {
     div = document.createElement('div');
     div.className = 'card';
+    div.addEventListener('click', function(event){
+      this.querySelector(".number").style.display = "block";
+      this.querySelector(".suit").style.display = "block";
+    });
     var ascii_char;
     if(myDeck[i].suit =='Diamonds'){
       ascii_char = '&diams;';
